490a3097386b2e2a78d8eb9453fd8a368a1f56ab
Keva Newman <keva@K-BOX.local>
2020-10-15 14:05:29 -0700

    testing contributions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,60 +40,60 @@ class Navbar extends Component {
                     </div>
                 </nav>
 
-    <div className="h-screen flex overflow-hidden bg-gray-100">
-        <div className="md:hidden">
-            <div className="fixed inset-0 flex z-40">
-                <div className="fixed inset-0">
-                    <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
+                <div className="h-screen flex overflow-hidden bg-gray-100">
+                    <div className="md:hidden">
+                        <div className="fixed inset-0 flex z-40">
+                            <div className="fixed inset-0">
+                                <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
+                            </div>
+
+                            <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
+                                <div class="absolute top-0 right-0 -mr-14 p-1">
+                                    <button class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" aria-label="Close sidebar">
+                                        <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                                <div class="mt-5 flex-1 h-0 overflow-y-auto">
+                                    <nav class="px-2 space-y-1 ">
+                                        <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
+                                            <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
+                                            </svg>
+                                            Home
+                                            </div>
+                                        <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
+                                            <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <Navitem item="Experiences" tolink="/experiences" activec={this.activeitem}></Navitem>
+                                            </svg>
+                                            Experiences
+                                            </div>
+                                        <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
+                                            <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <Navitem item="Education" tolink="/education" activec={this.activeitem}></Navitem>
+                                            </svg>
+                                            Education
+                                            </div>
+                                        <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
+                                            <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <Navitem item="Skills" tolink="/skills" activec={this.activeitem}></Navitem>
+                                            </svg>
+                                            Skills
+                                            </div>
+                                        <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
+                                            <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <Navitem item="Contacts" tolink="/contacts" activec={this.activeitem}></Navitem>
+                                            </svg>
+                                            Contacts
+                                            </div>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            
-        <div class="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
-        <div class="absolute top-0 right-0 -mr-14 p-1">
-          <button class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" aria-label="Close sidebar">
-            <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-      
-        <div class="mt-5 flex-1 h-0 overflow-y-auto">
-          <nav class="px-2 space-y-1 ">
-          <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
-              <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <Navitem item="Home" tolink="/" activec={this.activeitem}></Navitem>
-            </svg>
-              Home
-            </div>
-            <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
-              <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <Navitem item="Experiences" tolink="/experiences" activec={this.activeitem}></Navitem>
-            </svg>
-              Experiences
-            </div>
-            <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
-              <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <Navitem item="Education" tolink="/education" activec={this.activeitem}></Navitem>
-            </svg>
-              Education
-            </div>
-            <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
-              <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <Navitem item="Skills" tolink="/skills" activec={this.activeitem}></Navitem>
-            </svg>
-              Skills
-            </div>
-            <div href="#" class="group flex items-center px-2 py-2 text-base leading-6 font-medium text-gray-900 rounded-md bg-teal-500 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150">
-              <svg class="mr-4 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <Navitem item="Contacts" tolink="/contacts" activec={this.activeitem}></Navitem>
-            </svg>
-              Contacts
-            </div>     
-            </nav>
-        </div>
-      </div>   
-      </div>
-      </div>  
-      </div>            
             </>
         );
     }
